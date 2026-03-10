f6ce6ccadec9e0009af8ead53c975751f3dda9c4
Adarsh Singh <adarshsikarwar543@gmail.com>
2026-03-10 23:26:10 +0530

    day5 1 task commpeleted

---
+++ Day1RevisionInDay5/server.js
@@ -0,0 +1,19 @@
+import express from "express";
+
+const app = express();
+
+app.use(express.json());
+
+const notes = [];
+
+app.post("/notes", (req, res) => {
+  const note = req.body;
+  notes.push(note);
+  res.status(201).json({
+    message: "Note created successfully",
+  });
+});
+
+app.listen(3000, (req, res) => {
+  console.log("Server is running on port 3000");
+});
---
+++ Day2RevisionINDay5/server.js
@@ -0,0 +1,37 @@
+import express from "express";
+
+const app = express();
+
+app.use(express.json());
+
+const notes = [];
+
+app.post("/notes", (req, res) => {
+  const note = req.body;
+  notes.push(note);
+  res.status(201).json({
+    message: "Note created successfully",
+  });
+});
+
+app.get("/notes", (req, res) => {
+  res.status(200).json({
+    message: "note fetched successfully",
+    notes,
+  });
+});
+
+app.delete("/notes/:index", (req, res) => {
+  const { index } = req.params;
+  delete notes[index];
+
+  res.status(200).json({
+    message: "note deleted successfully",
+  });
+});
+
+app.patch("/notes/:index", (req, res) => {
+  const { index } = req.params;
+  const { title, description } = req.body;
+
+  
